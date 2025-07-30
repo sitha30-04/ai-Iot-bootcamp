9f5b8a66e6328058f2a0c55298519bdf5cfb0c1a
sitha30-04 <minnekantisitha@gmail.com>
2025-07-30 14:14:07 +0530

    adding files

---
+++ app1.py
@@ -0,0 +1,13 @@
+name="MLWEC"
+number=10.0
+isOn=True
+letter="H"
+number -=10
+# Arithemetic- + - * / %
+#logic - and,or,not
+#comparison- <,>,==,!=,<=,>=
+#assignment - =,+=,-=,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(isOn)
---
+++ app2.py
@@ -0,0 +1,7 @@
+# write a code whether a number is even or odd
+number=int(input("enter a number:"))
+if number%2==0:
+    print("even")
+else:
+    print("odd")
+
